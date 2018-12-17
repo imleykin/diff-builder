577e63faf32864132436133d5b795261ea20e31f
Ilya Leykin <imleykin@gmail.com>
2018-12-17 22:30:05 +0500

    main module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-export default (a, b) => a + b;
+import fs from 'fs';
+import path from 'path';
+import getParsedObject from './getParsedObject';
+import getAST from './getAST';
+import render from './renderers';
+
+const genDiff = (beforePath, afterPath, outputFormat = 'pretty') => {
+  const beforeFile = fs.readFileSync(beforePath);
+  const afterFile = fs.readFileSync(afterPath);
+
+  const beforeFileType = path.extname(beforePath);
+  const afterFileType = path.extname(afterPath);
+
+  const beforeObject = getParsedObject(beforeFile, beforeFileType);
+  const afterObject = getParsedObject(afterFile, afterFileType);
+
+  const diffAST = getAST(beforeObject, afterObject);
+
+  console.log(diffAST);
+
+  return render(diffAST, outputFormat);
+};
+
+export default genDiff;

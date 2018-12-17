50a6213cd3f73821585ab959cb8d22cf158e5e1c
Ilya Leykin <imleykin@gmail.com>
2018-12-17 22:28:12 +0500

    updated tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -3,11 +3,15 @@ import genDiff from '../src';
 
 const fixturesPath = './__tests__/__fixtures__/';
 
-test('basic test', () => {
-  const resultPath = `${fixturesPath}result.txt`;
+test('test input JSON', () => {
   const beforeJSONPath = `${fixturesPath}before.json`;
   const afterJSONPath = `${fixturesPath}after.json`;
-  const result = fs.readFileSync(resultPath, 'utf8');
 
-  expect(genDiff(beforeJSONPath, afterJSONPath)).toBe(result);
+  const resultJSONPath = `${fixturesPath}resultJSON.txt`;
+  const resultJSON = fs.readFileSync(resultJSONPath, 'utf8').replace('\n', '');
+  expect(genDiff(beforeJSONPath, afterJSONPath, 'json')).toBe(resultJSON);
+
+  const resultPrettyPath = `${fixturesPath}resultPretty.txt`;
+  const resultPretty = fs.readFileSync(resultPrettyPath, 'utf8');
+  expect(genDiff(beforeJSONPath, afterJSONPath, 'pretty')).toBe(resultPretty);
 });

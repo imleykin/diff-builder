cb2c22ebb4608a09e7b04f8bbea77af9b18c0672
Ilya Leykin <imleykin@gmail.com>
2019-06-21 20:44:47 +0500

    fire gendiff function in bin

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-
 import program from 'commander';
+import genDiff from '..';
 
 program
   .version('0.1.0', '-V, --version')
@@ -8,6 +8,6 @@ program
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format')
   .action((firstConfig, secondConfig) => {
-    console.log(program.format, program.firstConfig, secondConfig);
+    console.log(genDiff(firstConfig, secondConfig, program.format));
   })
   .parse(process.argv);
